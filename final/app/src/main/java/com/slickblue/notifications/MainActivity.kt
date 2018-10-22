6a5eb66bf887c544fc1788a68faf002ce648d66f
lafrance <green_ig1na@hotmail.fr>
2018-10-22 18:53:47 +0200

    clean final code and update

--- final/app/src/main/java/com/slickblue/notifications/MainActivity.kt
+++ final/app/src/main/java/com/slickblue/notifications/MainActivity.kt
@@ -13,30 +13,46 @@ import android.support.v4.app.NotificationManagerCompat
 import android.view.View
 import android.widget.Button
 
+//Nous instancions ici des constantes nécessaires à la création de notre notification, leur valeur importe peu pour ce TP.
 const val CHANNEL_ID = "com.slickblue.channel_id"
 const val notif_1_ID = 1
 
 class MainActivity : AppCompatActivity() {
 
-
-    var textTitle = "Ma notification"
-    var textContent = "Ceci est une notification"
-    var channel_name = "channel name"
-    var channel_description = "channel description"
-
-    lateinit var mBuilder: NotificationCompat.Builder
+    //On instancie les variables nécessaires à la construction de notre notification. Leur valeur importe peu.
+    val textTitle = "Ma notification"
+    val textContent = "Ceci est une notification"
+    val channel_name = "channel name"
+    val channel_description = "channel description"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Nous appelons ici la fonction qui va créer le canal pour notre notification, ceci est indispensable depuis Android 8.0.
         createNotificationChannel()
 
+        //Nous créons ici notre bouton qui va nous permettre de créer et d'afficher notre notification.
+        val button1 = findViewById<Button>(R.id.notification_1)
+
+        //Nous ajoutons un listener pour afficher la notification lors d'un appui sur le bouton.
+        button1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                createNotification()
+            }
+        })
+    }
+
+    //Cette fonction se charge de créer et d'afficher la notification.
+    private fun createNotification() {
+        //Nous créons l'intent qui sera appelée lors d'un appui sur notre notification
         val intent = Intent(this, NotificationDetails::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
-        mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+        //Nous créons ici la notification et l'initialisons avec différentes fonctions et paramètres.
+        var mBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.heart)
                 .setContentTitle(textTitle)
                 .setContentText(textContent)
@@ -44,16 +60,16 @@ class MainActivity : AppCompatActivity() {
                         .bigText("Un très long text qu'il est impossible d'afficher sur une notification normale, nous devons donc l'étendre pour l'afficher. Bla bla bla bla blablablablablablablablablablablablab bla blab blablabablablablalb"))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
+                .addAction(R.drawable.heart, "ACTION", pendingIntent)
                 .setAutoCancel(true)
 
-        val button1 = findViewById<Button>(R.id.notification_1)
-        button1.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                showNotification1()
-            }
-        })
+        //Ici on affiche la notification
+        with(NotificationManagerCompat.from(this)) {
+            notify(notif_1_ID, mBuilder.build())
+        }
     }
 
+    //cette fonction va nous servir à créer un canal pour notre notification, obligatoire depuis Android 8.0
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = channel_name
@@ -65,12 +81,6 @@ class MainActivity : AppCompatActivity() {
             val notificationManager: NotificationManager =
                     getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    fun showNotification1() {
-        with(NotificationManagerCompat.from(this)) {
-            notify(notif_1_ID, mBuilder.build())
         }
     }
 }
